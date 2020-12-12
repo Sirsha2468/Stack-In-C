88fc032cf03bd386742c243beaa03a4861ec0314
Sirsha2468 <69218962+Sirsha2468@users.noreply.github.com>
2020-12-13 01:24:52 +0530

    Update Mystack.c

--- Programe/C Program/Mystack.c
+++ Programe/C Program/Mystack.c
@@ -1,1 +1,71 @@
+#include<stdio.h>
+#define MAX 10
 
+int STACK[MAX],TOP;
+
+void push(int x);
+int pop();
+
+	void main()
+	{
+
+	int data,choice;
+	
+	do
+
+	{
+		
+	printf("\n\t\t\t\tStack Using Array\n\n\t\t\t\t1.Push\n\n\t\t\t\t2.Pop\n\n\t\t\t\t3.Exit\n\n\t\t\t\tEnter your choice:");
+	scanf("%d",&choice);
+
+	switch(choice)
+
+	{
+		case 1:
+		printf("\n\t\t\tEnter value to push:");
+		scanf("%d",&data);
+		break;
+		
+		case 2:
+		printf("\n\t\t\tThe popped value is:%d",pop());
+		break;
+		
+		case 3:
+		break;
+		default:printf("\nEnter correct choice:");	
+	}
+
+	}
+	while(choice!=3);
+	}
+	void push(int x)
+	
+	{
+		
+		if(TOP==MAX-1)
+
+		printf("\nStack Overflow");
+
+		else
+
+		STACK[++TOP]=x;
+	
+	}
+	
+	int pop()
+	
+	{
+		if(TOP==-1)
+		
+		{
+		
+		printf("\nStack underflow");
+	
+		return 0;
+		
+		}
+		
+		else
+		
+		return STACK[TOP--];
+	}
